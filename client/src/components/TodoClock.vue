2b9bc00f4e516d71a8f454d1fbb3d0da14a314a8
minseong3 <alstjd1021@naver.com>
2025-02-03 13:10:10 +0900

    TodoClock.vue changed to script setup

--- client/src/components/TodoClock.vue
+++ client/src/components/TodoClock.vue
@@ -1,75 +1,42 @@
 <template>
-    <div>
+    <div class="container">
         <p>{{ date }}<br>
-        {{ampm}} {{ time }}</p>
+          {{ampm}} {{ time }}</p>
     </div>
 </template>
 
-<script>
-// Options API -> Composition API로 변경
+<script setup>
 import { ref, onMounted } from 'vue';
 
-export default {
-  setup() {
-    const ampm = ref('');
-    const time = ref('');
-    const date = ref('');
+const ampm = ref('');
+const time = ref('');
+const date = ref('');
 
-    function getNow() {
-      const now = new Date();
+function getNow() {
+  const now = new Date();
       
-      ampm.value = now.getHours() >= 12 ? 'PM' : 'AM';
+  ampm.value = now.getHours() >= 12 ? 'PM' : 'AM';
+  // || 쓰는 이유 : 12, 24 는 falsy한 값이기 때문
+  time.value = `${String(now.getHours() % 12 || 12).padStart(2, '0')}:${String(now.getMinutes()).padStart(2, '0')}:${String(now.getSeconds()).padStart(2, '0')}`;
 
-      time.value = `${String(now.getHours() % 12 || 12).padStart(2, '0')}:${String(now.getMinutes()).padStart(2, '0')}:${String(now.getSeconds()).padStart(2, '0')}`;
-
-      date.value = `${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, '0')}-${now.getDate().toString().padStart(2, '0')}`;
-    }
-
-    onMounted(() => {
-      getNow();
-      setInterval(getNow, 1000);
-    });
-
-    return {ampm, time, date};
-  }
+  date.value = `${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, '0')}-${now.getDate().toString().padStart(2, '0')}`;
 }
-// export default {
-//   components: {},
-//   data() {
-//     return {
-//       ampm: '',
-//       time: '',
-//       date: ''
-//     }
-//   },
-//   created() {
-//     setInterval(() => this.getNow(), 1000)
-//   },
-//   methods: {
-//     getNow() {
-//       const date = new Date()
-//       // 오전, 오후(AM, PM) 표시
-//       this.ampm = date.getHours() >= 12 ? 'PM' : 'AM'
-      
-//       // 시분초(00:00:00) 표시 || 쓰는 이유 : 12, 24 는 falsy한 값이기 때문
-//       this.time = `${String(date.getHours() % 12 || 12).padStart(2, '0')}:${String(
-//         date.getMinutes()
-//       ).padStart(2, '0')}:${String(date.getSeconds()).padStart(2, '0')}`
-      
-//       // 년월일(yyyy-mm-dd) 표시
-//       this.date = `${date.getFullYear()}-${(date.getMonth() + 1)
-//         .toString()
-//         .padStart(2, 0)}-${date.getDate().toString().padStart(2, 0)}`
-//     }
-//   }
-// }
+
+onMounted(() => {
+  getNow();
+  setInterval(getNow, 1000);
+});
 </script>
 
 <style scoped>
+    .container {
+      display: flex;
+      justify-content: center; /* 가로 중앙 정렬 */
+      align-items: center; /* 세로 중앙 정렬 */
+    }
     p {
         width: 165px; /* 너비 */
         height: 80px;
-        transform: translateX(850px);
         text-align: center;
         font-family: 'Ownglyph_StudyHard-Rg';
         font-size: 35px;
@@ -77,7 +44,7 @@ export default {
         justify-content: center;
         align-items: center;
         padding: 20px;
-        border-radius: 15px;
+        border-radius: 20px;
         background: rgba(255, 145, 0, 0.904); /* 투명한 배경 */
         box-shadow: 0 10px 30px rgba(0, 0, 0, 0.3); /* 그림자 */
         border: 5px solid #000000;
