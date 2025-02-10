0083603434e8235fa750d529957ab970a83156c2
minseong3 <alstjd1021@naver.com>
2025-02-10 17:01:47 +0900

    jajilgurae

--- server/src/main/java/todo/todominsung/module/dto/TodoTextUpdateDto.java
+++ server/src/main/java/todo/todominsung/module/dto/TodoTextUpdateDto.java
@@ -1,12 +1,10 @@
 package todo.todominsung.module.dto;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
 public class TodoTextUpdateDto {
     private String text;
 }
